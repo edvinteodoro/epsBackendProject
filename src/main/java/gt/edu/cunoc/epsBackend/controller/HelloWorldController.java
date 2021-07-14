2b7aa2bb3f767d5e749c52cdf8a37bebf4a6b981
Edvin Gonzalez <egonzalez@easygosa.net>
2021-07-13 21:56:36 -0600

    add controller to project

---
+++ src/main/java/gt/edu/cunoc/epsBackend/controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.edu.cunoc.epsBackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author teodoro
+ */
+@RestController
+public class HelloWorldController {
+    
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello javaTpoint";
+    }
+    
+}
