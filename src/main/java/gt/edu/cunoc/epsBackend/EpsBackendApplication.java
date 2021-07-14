faf3b08686bcc537563e35e668e8c53392335e6f
Edvin Gonzalez <egonzalez@easygosa.net>
2021-07-13 20:31:15 -0600

    initialize project

---
+++ src/main/java/gt/edu/cunoc/epsBackend/EpsBackendApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.cunoc.epsBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gt/edu/cunoc/epsBackend/EpsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.edu.cunoc.epsBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
